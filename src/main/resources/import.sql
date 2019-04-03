a1a5affd229c98eabaa78bf0f4b502a54c6f0401
Seyfullah <seyfullah.semen@hotmail.com>
2019-04-03 21:01:50 +0200

    Added a Seeder so it will populate the database with data after startup.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO course
+VALUES (1, 'Architecture and Design'),
+       (2, 'Research design');
+
+INSERT INTO lesson
+VALUES (1, 'Week 1'),
+       (2, 'Week 2');
+
+INSERT INTO lesson_slides
+VALUES (1, 'This is the first slide for week 1', 1),
+       (2, 'This is the second slide for week 1', 1),
+       (3, 'This is the first slide for week 2', 2),
+       (4, 'Yet another boring slide for week 2', 2);
